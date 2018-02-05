a1045c5d6e07e1a975856dbea7835ca26bb7f6e5
LIORANJITH822 <35991661+LIORANJITH822@users.noreply.github.com>
2018-02-05 14:10:27 +0530

    Create ranji26.c

---
+++ ranji26.c
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void main()
+{
+    size_t len = rand() % 25 + 5;
+
+    std::string x;
+
+    std::generate_n(std::back_inserter(x), len, rand);
+    return x;
+}
+
+static const int num = 10000000;
+
+int main(){
+    std::vector<std::string> strings;
+
+    std::generate_n(std::back_inserter(strings), num, gen_random);
+
+    clock_t start = clock();
+    std::sort(strings.begin(), strings.end());
+    clock_t ticks = clock() - start;
+
+    std::cout << ticks / (double)CLOCKS_PER_SEC;
+
+    getch();
+}
